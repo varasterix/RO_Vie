5878e7ef40ae78deceed31ec19ecde14fbed9265
thibaut1998e <thibaut.eloy@imt-atlantique.net>
2019-12-17 13:46:07 +0100

    test

--- solution_crossing.py
+++ solution_crossing.py
@@ -1,3 +1,15 @@
 def crossing(flowshop, initial_pop):
     population = initial_pop
     return population
+
+
+def crossing2(flowshop, initial_pop):
+    """ Règle de croisement : pour chaque job on fait la somme de leurs position et on place les jobs par ordre croissant de
+    leur position : ma fonction part du principe que la population initial est paire """
+    return population
+
+def crossingIndividuals(ordo1, ordo2):
+    """croise 2 ordonnancements et retourne l'ordo fils"""
+
+    for i in range(len(ordo1.seq)):
+        lis
