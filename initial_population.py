edbadf31ec2c398ebda048fea6ec75593d467b3c
Agathe <blu.kannely@gmail.com>
2020-01-06 11:23:20 +0100

    deterministic population can be selected by duration rather than randomly in initial_population.py

--- initial_population.py
+++ initial_population.py
@@ -6,37 +6,45 @@ from ordonnancement import Ordonnancement
 MAXINT = 10000
 
 
-def initial_pop(flow_shop, deter_prop, random_prop):
+def initial_pop(flow_shop, random_prop, deter_prop, best_deter=False, pop_init_size=100):
     """
     Generates the initial population following a proportion of deterministic and random population
     :param flow_shop: an instance of the flow shop permutation problem
-    :param deter_prop: proportion of the initial population computed in a deterministic manner
-    :param random_prop: proportion of the initial population randomly generated
+    :param deter_prop: desired proportion of the initial population computed in a deterministic manner
+    :param random_prop: desired proportion of the initial population randomly generated
+    :param best_deter: if true, select deterministic initial pop by scheduling duration, else random selection
+    :param pop_init_size: size of the initial population, if bigger than the size of the total population, redefined to 1/3 of the total population
     :return: the initial population for the memetic algorithm
     """
     pop_max_size = math.factorial(flow_shop.nb_jobs)
-    pop_init_size = 100
-    if pop_max_size < 100:
-        pop_init_size = round(pop_max_size * 1/3)
-    deter_size = round(deter_prop / (deter_prop+random_prop) * pop_init_size)
-    rdm_size = pop_init_size - deter_size
-    rdm_pop = random_initial_pop(flow_shop, rdm_size)
-    deter_pop = deterministic_initial_pop(flow_shop, deter_size)
+    if pop_max_size < pop_init_size:
+        pop_init_size = round(pop_max_size * 1 / 3)
+
+    deter_size = round(deter_prop / (deter_prop + random_prop) * pop_init_size)
+    deter_pop = []
+    if deter_size != 0:
+        deter_pop = deterministic_initial_pop(flow_shop, deter_size, best_deter)
+
+    rdm_size = pop_init_size - len(deter_pop)
+    rdm_pop = []
+    if rdm_size != 0:
+        rdm_pop = random_initial_pop(flow_shop, rdm_size)
+
     starting_pop = rdm_pop + deter_pop
     return starting_pop
 
 
-def random_initial_pop(flow_shop, nb_value=10):
+def random_initial_pop(flow_shop, rdm_size):
     """
     Generates randomly the initial population
     :param flow_shop: an instance of the flow shop permutation problem
-    :param nb_value: number of element in the initial population to generate
-    :return population: the random part of the initial population for the memetic algorithm
+    :param rdm_size: number of element in the initial population to generate
+    :return: the random part of the initial population for the memetic algorithm
     """
     population_seq = []
     population = []
     start = [flow_shop.liste_jobs(i) for i in range(flow_shop.nb_jobs)]
-    for i in range(nb_value):
+    for i in range(rdm_size):
         random.shuffle(start)
         elem = copy.copy(start)
         population_seq.append(elem)
@@ -47,38 +55,13 @@ def random_initial_pop(flow_shop, nb_value=10):
     return population
 
 
-def johnson_rule_order(flow_shop, sum_index):
-    list_to_order = []
-
-    for j in flow_shop.l_job:
-        start = 0
-        end = 0
-        for i in range(0, sum_index):
-            start += j.duree_operation(i)
-        for i in range(sum_index, flow_shop.nb_machines):
-            end += j.duree_opeation(i)
-        list_to_order.append((j, start, end))
-    john_start = []
-    john_end = []
-    while len(list_to_order) != 0:
-        min_start = min(list_to_order, key=lambda t: t[1])
-        min_end = min(list_to_order, key=lambda t: t[2])
-        if min_start[1] < min_end[2]:
-            john_start.append(min_start[0])
-            list_to_order.remove(min_start)
-        else:
-            john_end.insert(0, min_end[0])
-            list_to_order.remove(min_end)
-    johnson_order = john_start + john_end
-    return johnson_order
-
-
-def deterministic_initial_pop(flow_shop, nb_value=10):
+def deterministic_initial_pop(flow_shop, deter_size, best_deter):
     """
-
-    :param flow_shop:
-    :param nb_value:
-    :return deter_pop:
+    Generates deterministically the initial population
+    :param flow_shop: an instance of the flow shop permutation problem
+    :param deter_size: number of element in the initial population to generate
+    :param best_deter:
+    :return:
     """
     all_deterministic_seq = []
     neh_seq = neh_order(flow_shop)
@@ -86,16 +69,27 @@ def deterministic_initial_pop(flow_shop, nb_value=10):
     for m_index in range(flow_shop.nb_machines):
         temp_sep_desc = job_duration_order_desc(flow_shop, m_index)
         temp_sep_asc = job_duration_order_asc(flow_shop, m_index)
-        all_deterministic_seq.append(temp_sep_asc, temp_sep_desc)
-    for sum_index in range(flow_shop.nb_machines-1):
+        all_deterministic_seq.append(temp_sep_asc)
+        all_deterministic_seq.append(temp_sep_desc)
+    for sum_index in range(flow_shop.nb_machines - 1):
         temp_johnson_seq = johnson_rule_order(flow_shop, sum_index)
         all_deterministic_seq.append(temp_johnson_seq)
-    seq_deter_sample = random.sample(all_deterministic_seq, nb_value)
+
     deter_pop = []
-    for seq in seq_deter_sample:
-        ordo = Ordonnancement(flow_shop.nb_machines)
-        ordo.ordonnancer_liste_job(seq)
-        deter_pop.append(ordo)
+    if best_deter:
+        all_deterministic_ordo = []
+        for seq in all_deterministic_seq:
+            ordo = Ordonnancement(flow_shop.nb_machines)
+            ordo.ordonnancer_liste_job(seq)
+            all_deterministic_ordo.append(ordo)
+        sorted_scheduling = sorted(all_deterministic_ordo, key=lambda o: o.duree(), reverse=True)
+        deter_pop = sorted_scheduling[0:deter_size]
+    else:
+        seq_deter_sample = random.sample(all_deterministic_seq, deter_size)
+        for seq in seq_deter_sample:
+            ordo = Ordonnancement(flow_shop.nb_machines)
+            ordo.ordonnancer_liste_job(seq)
+            deter_pop.append(ordo)
     return deter_pop
 
 
@@ -103,9 +97,9 @@ def neh_order(flow_shop):
     """
     Compute the NEH scheduling of flow_shop
     :param flow_shop: an instance of the flow shop permutation problem
-    :return best_order: return the NEH scheduling of flow_shop
+    :return: return the NEH scheduling of flow_shop
     """
-    sorted_jobs = sorted(flow_shop.l_job, key=lambda job: job.duree(), reverse=True)
+    sorted_jobs = sorted(flow_shop.l_job, key=lambda j: j.duree(), reverse=True)
     best_order = []
     for job in sorted_jobs:
         min_duration = MAXINT
@@ -122,12 +116,44 @@ def neh_order(flow_shop):
     return best_order
 
 
+def johnson_rule_order(flow_shop, sum_index):
+    """
+    Schedule a flow shop following the Johnson rule
+    :param flow_shop: an instance of the flow shop permutation problem
+    :param sum_index: splinting tasks in two groups at this index
+    :return:
+    """
+    list_to_order = []
+    for j in flow_shop.l_job:
+        start = 0
+        end = 0
+        for i in range(0, sum_index):
+            start += j.duree_operation(i)
+        for i in range(sum_index, flow_shop.nb_machines):
+            end += j.duree_opeation(i)
+        list_to_order.append((j, start, end))
+
+    john_start = []
+    john_end = []
+    while len(list_to_order) != 0:
+        min_start = min(list_to_order, key=lambda t: t[1])
+        min_end = min(list_to_order, key=lambda t: t[2])
+        if min_start[1] < min_end[2]:
+            john_start.append(min_start[0])
+            list_to_order.remove(min_start)
+        else:
+            john_end.insert(0, min_end[0])
+            list_to_order.remove(min_end)
+    johnson_order = john_start + john_end
+    return johnson_order
+
+
 def job_duration_order_desc(flow_shop, m_index):
     """
-
-    :param flow_shop:
-    :param m_index:
-    :return sorted_jobs:
+    Schedule a flow shop following the duration of each job on the reference machine (descending order)
+    :param flow_shop: an instance of the flow shop permutation problem
+    :param m_index: index of the reference machine
+    :return: return the computed schedule
     """
     sorted_jobs = sorted(flow_shop.l_job, key=lambda job: job.duree_operation(m_index), reverse=True)
     return sorted_jobs
@@ -135,10 +161,10 @@ def job_duration_order_desc(flow_shop, m_index):
 
 def job_duration_order_asc(flow_shop, m_index):
     """
-
-    :param flow_shop:
-    :param m_index:
-    :return sorted_jobs:
+    Schedule a flow shop following the duration of each job on the reference machine (ascending order)
+    :param flow_shop: an instance of the flow shop permutation problem
+    :param m_index: index of the reference machine
+    :return: return the computed schedule
     """
     sorted_jobs = sorted(flow_shop.l_job, key=lambda job: job.duree_operation(m_index), reverse=False)
     return sorted_jobs
