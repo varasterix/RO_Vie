2d41ffcf2c1cf3dfca5109d57e9e4a1a7f2e57a1
carlivoix <charlesvivoix@gmail.com>
2020-01-07 15:19:40 +0100

    crossover 1 point + test

--- test/test_solution_crossover.py
+++ test/test_solution_crossover.py
@@ -1,5 +1,5 @@
 import unittest
-from solution_crossover import crossover_2_points, crossover
+from solution_crossover import crossover_2_points, crossover, crossover_1_point
 from flowshop import Flowshop
 from job import Job
 from ordonnancement import Ordonnancement
@@ -26,6 +26,20 @@ class TestSolutionCrossoverFileMethods(unittest.TestCase):
             for job in [job_1, job_2, job_3, job_4, job_5]:
                 self.assertIn(job, sched.sequence())
         # TODO
+
+    def test_crossover_1_points(self):
+        parent_1 = Ordonnancement(job_1.nb_op)
+        parent_2 = Ordonnancement(job_1.nb_op)
+        parent_1.ordonnancer_liste_job([job_2, job_3, job_4, job_5, job_1])
+        parent_2.ordonnancer_liste_job([job_1, job_4, job_5, job_2, job_3])
+        initial_pop = [parent_1, parent_2]
+        new_pop = crossover_1_point(parent_1, parent_2, 3)
+        self.assertEqual(len(initial_pop), len(new_pop))
+        for sched in new_pop:
+            self.assertEqual(len(sched.sequence()), 5)
+            self.assertEqual(sched.has_duplicate(), False)
+            for job in [job_1, job_2, job_3, job_4, job_5]:
+                self.assertIn(job, sched.sequence())
 
     def test_crossover(self):
         parent_1 = Ordonnancement(job_1.nb_op)
