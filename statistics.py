b6c693b676f6909e036199c8202ff25a542d802a
Agathe <blu.kannely@gmail.com>
2020-01-10 15:39:39 +0100

    statistics.py debug

--- statistics.py
+++
@@ -1,6 +0,0 @@
-import statistics
-
-
-def population_statistics(pop):
-    pop_duration = [sched.duree() for sched in pop]
-    return statistics.mean(pop_duration), min(pop_duration), max(pop_duration)
