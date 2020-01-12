d0161e72e5c9f5f52dd49b1543d67c264fbf2a9e
Emilie Lhomme <emilie.lhomme@laposte.net>
2020-01-12 10:46:26 +0100

    gest_best_parameters.py is modified

--- get_best_parameters.py
+++ get_best_parameters.py
@@ -12,10 +12,9 @@ if __name__ == "__main__":
     for dataSet in os.listdir(data_path):
         for instance in os.listdir(data_path + dataSet):
             file_name = instance.split('.txt')[0]
-            if file_name == 'tai52':
-                print("Grid search for instance " + data_path + dataSet + '/' + file_name + '...')
-                flow_shop_file_path = data_path + dataSet + "/" + instance
-                grid_search_analysis_args = ["python", python_script, grid_search_parameters_path, flow_shop_file_path,
-                                             store_best_parameters_path]
-                subprocess.call(args=grid_search_analysis_args)
+            print("Grid search for instance " + data_path + dataSet + '/' + file_name + '...')
+            flow_shop_file_path = data_path + dataSet + "/" + instance
+            grid_search_analysis_args = ["python", python_script, grid_search_parameters_path, flow_shop_file_path,
+                                         store_best_parameters_path]
+            subprocess.call(args=grid_search_analysis_args)
     print("Finished!")
