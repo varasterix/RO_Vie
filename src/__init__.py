9202e064b1a9d4a0a3348b4eb6c90371a98bfcb2
Emilie Lhomme <emilie.lhomme@laposte.net>
2020-01-13 12:52:35 +0100

    __init__.py files are updated in src/ and test/

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
-from src import constants, convergence, flowshop, initial_population, job, ordonnancement, population_statistics, \
-    solution_crossover, visualisation, utils
+from src import constants, convergence, flowshop, initial_population, job, local_search, memetic, mutation, \
+    ordonnancement, population_statistics, solution_crossover, utils, visualisation
