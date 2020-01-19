19c8f35dd95e4b844eefeebeaf29737abac29960
Agathe <blu.kannely@gmail.com>
2020-01-19 18:54:01 +0100

    tai41 : Best known: 2991, Best found 3064 (2.44%)

--- compute_unit.py
+++ compute_unit.py
@@ -2,7 +2,7 @@ import subprocess
 import src.constants as constants
 
 time_limit = 10
-flow_shop_file_path = 'data/dataset1/tai31.txt'
+flow_shop_file_path = 'data/dataset1/tai41.txt'
 parameters_file_path = constants.PARAMETER_DEFAULT_PARAMETERS_FILE_PATH
 global_memetic_results_path = constants.PARAMETER_GLOBAL_MEMETIC_RESULTS_PATH
 visualise_results = 1  # boolean
