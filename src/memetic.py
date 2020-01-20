dc9f5634c2bc8cc5ee18d8fa0c3e5f4d7726827f
carlivoix <charlesvivoix@gmail.com>
2020-01-20 14:08:19 +0100

    debug convergence

--- src/memetic.py
+++ src/memetic.py
@@ -113,7 +113,7 @@ def memetic_heuristic(flowshop, parameters):
                 max([list_statistics[k][1] for k in range(max(0, len(list_statistics)-10), len(list_statistics))]):
             restart = True
 
-        if is_convergent(population[:20], threshold=entropy_threshold) or restart:
+        if is_convergent(population, threshold=entropy_threshold) or restart:
             iterations_where_restart.append(index)
             population = restart_population(population,
                                             flowshop,
